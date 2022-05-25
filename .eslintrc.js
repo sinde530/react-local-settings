a9f0e840a9d48c580a993a5f307c2ae1b51fc330
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-25 17:02:38 +0900

    .eslintrc.js modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,16 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/react-in-jsx-scope': 0,
+    'eslint-plugin-react': 0,
+    'react/prop-types': 0,
+    'react/button-has-type': 0,
+    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+    'react/jsx-no-bind': 'off',
+    'no-shadow': 'off',
+    'no-unused-expressions': 'off',
   },
 };
